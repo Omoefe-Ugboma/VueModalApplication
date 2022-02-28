d40051ee9feac4a877c1302990b6b3281932e762
Omoefe Ugboma <Omoefe.Ugboma@investment-one.com>
2022-02-28 20:15:46 +0100

    Vue Modal application

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div v-if="showModal">
+      <h1>{{ title }}</h1>
+  <!-- <input type="text" ref="name"> -->
+  <!-- <button @click="handleClick">CLick Me</button> -->
+     <!-- <Modal :header="header" :text="text" theme="sale"
+     @close="toggleModal"/>    -->
+     <modal theme="sale" @close="toggleModal">
+        <template v-slot:links>
+           <a href="#">Sign up now</a>
+           <a href="#">more info here please</a>
+        </template>
+        <h1>Welcome to FlexiconTech</h1>
+        <p>You will be very much excited learning here!</p>
+     </modal>
+  </div>
+  <button @click="toggleModal">open modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
+
 
 export default {
+  components: { Modal },
   name: 'App',
-  components: {
-    HelloWorld
-  }
+ data(){
+   return{
+     title:'My new vue tutorial',
+     header:'Sign up for the giveaway',
+     text:'Grab your Buhari new bag at the cheapest price!',
+     showModal: false
+   }
+ },
+ methods:{
+   handleClick(){
+     console.log(this.$refs.name)
+     this.$refs.name.classList.add('active')
+     this.$refs.name.focus()
+
+   },
+   toggleModal(){
+     this.showModal = !this.showModal
+   }
+ }
 }
 </script>
 
@@ -22,5 +54,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+p, h1{
+  margin-top: 23px;
+  color: white;
 }
 </style>
